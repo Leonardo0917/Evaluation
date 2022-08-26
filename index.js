71204b24631672185b7ef02e6bcd9a34f7b920a7
Neo Chen <tim839172010@gmail.com>
2022-08-26 14:24:34 -0700

    first version
    working selecting courses and button submit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { Api } from './api.js'
 const View = (() => {
     const domstr = {
         courses: ".courses",
+        available: "#available",
+        credits: ".credits",
     }
 
     const render = (ele, content) => {
@@ -15,10 +17,10 @@ const View = (() => {
         arr.forEach(element => {
             let type = element.required ? "Compulsory" : "Elective"; 
             tmp +=`
-                <li>
+                <li class="course">
                     <span>${element.courseName}</span>
                     <span>Course Type: ${type}</span>
-                    <span>Course Credit: ${element.credit}</span>
+                    <span>Course Credit: <span class="cc">${element.credit}</span></span>
                 </li>
             `;
         });
@@ -33,12 +35,10 @@ const View = (() => {
 })();
 // -----Model------
 const Model = ((api, view) => {
-    class course {
-        
-    }
-
     class State {
         #coursesList = [];
+        #selectedList = [];
+        #totalCredits = 0;
 
         get coursesList() {
             return this.#coursesList;
@@ -50,6 +50,26 @@ const Model = ((api, view) => {
             const courseContainer = document.querySelector(view.domstr.courses);
             const tagContent = view.createLists(this.#coursesList);
             view.render(courseContainer, tagContent);
+        }
+
+        get selectedList() {
+            return this.#selectedList;
+        }
+
+        set selectedList(newselectedList) {
+            this.#selectedList = [...newselectedList];
+        }
+
+        get totalCredits () {
+            return this.#totalCredits;
+        }
+
+        set totalCredits(num) {
+            this.#totalCredits = num;
+
+            const courseContainer = document.querySelector(view.domstr.credits);
+            let str = this.#totalCredits + " ";
+            view.render(courseContainer, str);
         }
     }
 
@@ -65,6 +85,22 @@ const Model = ((api, view) => {
 const Controller = ((model, view) => {
     const state = new model.State();
 
+    const selectCourse = () => {
+        const courses = document.querySelector(view.domstr.available);
+        courses.addEventListener('click', (event)=> {
+            console.log(event.target);
+            if (event.currentTarget.className === "course") {
+                let c = parseInt(event.target.querySelector(".cc").innerHTML);
+                console.log(c);
+                if (state.totalCredits + c > 18) {
+                    alert("You can only choose up to 18 credits in one semester");
+                } else {
+                    state.totalCredits = state.totalCredits + c;
+                }
+            }
+        })
+    }
+
     const init = () => {
         model.getCourses().then((lists) => {
             state.coursesList = [...lists];
@@ -73,6 +109,7 @@ const Controller = ((model, view) => {
 
     const bootstramp = () => {
         init();
+        selectCourse();
     };
 
     return {
