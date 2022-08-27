b04c1ad4f86d43c4758f7bdd8a2048713fd4a4d2
Neo Chen <tim839172010@gmail.com>
2022-08-26 20:18:47 -0700

    fix event-listener capturing

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const View = (() => {
         courses: ".courses",
         available: "#available",
         credits: ".credits",
+        sumbit: "#submit"
     }
 
     const render = (ele, content) => {
@@ -17,10 +18,10 @@ const View = (() => {
         arr.forEach(element => {
             let type = element.required ? "Compulsory" : "Elective"; 
             tmp +=`
-                <li>
-                    <span  class="course">${element.courseName}</br>
-                    Course Type: ${type}</br>
-                    Course Credit: <span class="cc">${element.credit}</span></span>
+                <li class="course">
+                    <span class="cn">${element.courseName}</span>
+                    <span>Course Type: ${type}</span>
+                    <span>Course Credit: <span class="cc">${element.credit}</span><span>
                 </li>
             `;
         });
@@ -35,6 +36,12 @@ const View = (() => {
 })();
 // -----Model------
 const Model = ((api, view) => {
+    class SelectedCourse {
+        constructor(name) {
+            this.name = name;
+        }
+    }
+
     class State {
         #coursesList = [];
         #selectedList = [];
@@ -77,6 +84,7 @@ const Model = ((api, view) => {
 
     return {
         getCourses,
+        SelectedCourse,
         State,
     };
 })(Api, View);
@@ -85,25 +93,7 @@ const Model = ((api, view) => {
 const Controller = ((model, view) => {
     const state = new model.State();
     const maxCredits = 18;
-
-    const selectCourse = () => {
-        const courses = document.querySelector(view.domstr.available);
-        courses.addEventListener('click', (event) => {
-            // There is an issue with if-statement, some li doesn't respnd when clicked
-            console.log(event.target)
-            if (event.target.className === "course") {
-                let cc = parseInt(event.target.querySelector(".cc").innerHTML);
-
-                if (state.totalCredits + cc > maxCredits) {
-                    alert("You can only choose up to 18 credits in one semester");
-                } else {
-                    state.totalCredits = state.totalCredits + cc;
-                    // update the selected li background-color
-                    
-                }
-            }
-        })
-    }
+    const selectedColor = "rgb(66, 171, 246)";
 
     const init = () => {
         model.getCourses().then((lists) => {
@@ -111,9 +101,70 @@ const Controller = ((model, view) => {
         });
     }
 
+    const selectCourse = () => {
+        const courses = document.querySelector(view.domstr.available);
+        courses.addEventListener("mouseup", (event) => {
+            let target;
+            event.stopPropagation();
+            // handle different layer, make <li> as target
+            if (event.target.className === "course") {
+                target = event.target;
+            } else if (event.target.className === "cc") {
+                target = event.target.parentElement.parentElement;
+            } else {
+                target = event.target.parentElement;
+            }
+
+            let cc = parseInt(target.querySelector(".cc").innerHTML);
+            let cn = target.querySelector(".cn").innerHTML;
+            
+            // unmark <li>
+            if (state.selectedList.some(e => e.name === cn)) {
+                state.selectedList = state.selectedList.filter(e => e.name !== cn);
+                state.totalCredits = state.totalCredits - cc;
+            } 
+            // mark <li>
+            else {
+                // check if valid
+                if (state.totalCredits + cc > maxCredits) {
+                    alert("You can only choose up to 18 credits in one semester");
+                } else {
+                    const selected_course = new model.SelectedCourse(cn);
+                    state.selectedList = [selected_course, ...state.selectedList];
+                    state.totalCredits = state.totalCredits + cc;
+                }
+            }
+            // refresh
+            const courseContainer = document.querySelector(view.domstr.courses);
+            const tagContent = view.createLists(state.coursesList);
+            view.render(courseContainer, tagContent);
+            // render bgcolor
+            const _course = document.querySelectorAll(".cn");
+            _course.forEach(e => {
+                for (let i = 0; i < state.selectedList.length; i++) {
+                    if (e.innerHTML === state.selectedList[i].name) {
+                        e.parentElement.style.backgroundColor = selectedColor;
+                        break;
+                    }
+                }
+            })
+        }, true);
+    }
+
+    const sumbit = () => {
+        const sumbitBtn = document.querySelector(view.domstr.sumbit);
+        sumbitBtn.addEventListener("click", (event) => {
+            let msg = "You have chosen " + state.totalCredits + " credits for this semester. You cannot change once you submit. Do you want to confirm?"
+            if(confirm(msg)) {
+                
+            }
+        });
+    }
+
     const bootstramp = () => {
         init();
         selectCourse();
+        sumbit();
     };
 
     return {
